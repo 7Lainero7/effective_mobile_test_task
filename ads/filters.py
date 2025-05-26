fa138a5ca9ebcd3c6f328bac825cffe97856c8ea
Lainero <7Lainero7@gmail.com>
2025-05-26 18:50:08 +0500

    added adfilter

---
+++ ads/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+
+from .models import Ad
+
+
+class AdFilter(django_filters.FilterSet):
+    """
+    Фильтр для объявлений по категории и состоянию.
+    """
+    category = django_filters.CharFilter(field_name='category', lookup_expr='iexact')
+    condition = django_filters.CharFilter(field_name='condition', lookup_expr='iexact')
+
+    class Meta:
+        model = Ad
+        fields = ['category', 'condition']
